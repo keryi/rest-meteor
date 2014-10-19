eee9eeb505fda52a3764ad5e983f5fec98e093e0
keryi <hti_32@yahoo.com>
2014-10-19 17:40:19 +0800

    Allow to post from web UI, added http package, UI is better looking now

--- client/views/posts/index.js
+++ client/views/posts/index.js
@@ -1,5 +1,38 @@
+Meteor.startup(function() {
+	Session.set('serverUrl', 'http://192.168.78.178:3000/')
+})
+
 Template.postIndex.helpers({
 	posts: function() {
 		return Posts.find();
+	}
+});
+
+Template.postSubmit.events({
+	'click #postCreate': function(e) {
+		e.preventDefault();
+
+		var content = $('#postContent').val();
+		if (content.length > 0) {
+			var dataObject = {
+				data: {
+					content: content,
+					created_at: new Date()
+				}
+			};
+
+			var url = Session.get('serverUrl') + 'api/v1/posts/';
+			console.log(url);
+			HTTP.post(url, dataObject, function(error, result){
+				
+				if (result) {
+					$('#postContent').val('');
+				}
+
+				if(error) {
+					console.error(error);
+				}
+			});
+		}
 	}
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,4 +1,14 @@
 if (Posts.find().count() == 0) {
-	Posts.insert({ content: 'This is a sample post' });
-	Posts.insert({ content: 'TGIF, lets grab some beers tonight' });
+	Posts.insert({
+		content: 'This is a sample post.',
+		created_at: new Date()
+	});
+	Posts.insert({
+		content: 'TGIF, lets grab some beers tonight.',
+		created_at: new Date()
+	});
+	Posts.insert({
+		content: 'I am going to watch a movie tonight. What are you going to do?',
+		created_at: new Date()
+	});
 }
