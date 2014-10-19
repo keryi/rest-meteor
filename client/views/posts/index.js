e10d59b2e0edc51c39ed9aa22f42a10fe6080488
keryi <hti_32@yahoo.com>
2014-10-19 22:16:43 +0800

    Allow to remove post

--- client/views/posts/index.js
+++ client/views/posts/index.js
@@ -8,6 +8,26 @@ Template.postIndex.helpers({
 	}
 });
 
+Template.postIndex.events({
+	'click .postDelete': function(e) {
+		e.preventDefault();
+
+		if (confirm('Are you sure you want to remove this post?')) {
+			var postId = $(e.currentTarget).attr('postId');
+
+			var url = Session.get('serverUrl') + 'api/v1/post/' + postId;
+
+			alert(url);
+
+			HTTP.del(url, {}, function(error, result){
+				if(error) {
+					console.error(error);
+				}
+			});
+		}
+	}
+});
+
 Template.postSubmit.events({
 	'click #postCreate': function(e) {
 		e.preventDefault();
@@ -22,7 +42,6 @@ Template.postSubmit.events({
 			};
 
 			var url = Session.get('serverUrl') + 'api/v1/posts/';
-			console.log(url);
 			HTTP.post(url, dataObject, function(error, result){
 				
 				if (result) {
